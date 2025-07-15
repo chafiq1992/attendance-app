5f6312f11448d6daa0963984e81a15d493d36dce
chafiq1992 <79022031+chafiq1992@users.noreply.github.com>
2025-07-15 01:24:07 +0100

    Fix JSX syntax

--- frontend/src/PeriodSummary.jsx
+++ frontend/src/PeriodSummary.jsx
@@ -208,7 +208,7 @@ export default function PeriodSummary() {
                     </div>
                   </div>
                 )
-              })() )}
+                })()}
             </div>
           )}
         </div>
